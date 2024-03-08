88b9da48ddef705381678d4c34080ab258df6266
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-03-08 10:45:06 +0500

    icons and router are set up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faBaseball } from '@fortawesome/free-solid-svg-icons';
+import {
+	faHtml5,
+	faCss3,
+	faBootstrap,
+	faReact,
+} from '@fortawesome/free-brands-svg-icons';
 function Home() {
-	return <div>Home</div>;
+	return (
+		<div className='container'>
+			<div className='top-row'>
+				<div className='columns-2'>
+					<FontAwesomeIcon className='html-icon' icon={faHtml5} />
+				</div>
+			</div>
+			<div className='icons-row'></div>
+		</div>
+	);
 }
 
 export default Home;
