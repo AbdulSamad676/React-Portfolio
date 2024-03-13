bb6389de2c7e8783dcf658ea597c934845c86d49
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-03-13 23:45:55 +0500

    services added successfully

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,29 +1,73 @@
+import {
+	faBootstrap,
+	faCss3,
+	faHtml5,
+	faReact,
+} from '@fortawesome/free-brands-svg-icons';
+import { faDesktop, faLaptopCode } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 function Services() {
 	return (
-		<div className='container mx-auto my-5 border border-black'>
+		<div className='container mx-auto my-10 '>
 			<h2 className='text-center my-2 py-2 px-5 text-2xl w-max font-bold text-white black-shadow custom-bg rounded-lg mx-auto'>
 				Services
 			</h2>
-			<div className='grid md:grid-cols-3 sm:grid-cols-2 gap-5 p-4'>
-				<div className='service-item p-4 rounded-lg text-white custom-bg border border-white'>
-					1
+			<div className='grid md:grid-cols-3 sm:grid-cols-2  gap-x-[5rem] md:gap-x-10 gap-y-5 p-4 md:w-10/12 mx-auto '>
+				<div className='service-item  h-32 p-4 rounded-lg text-white custom-bg border border-white'>
+					<FontAwesomeIcon
+						className='html-icon text-6xl hover:text-7xl'
+						icon={faHtml5}
+					/>
+					<h3 className='font-bold black-shadow text-xl '>
+						HTML Services
+					</h3>
 				</div>
-				<div className='service-item p-4 rounded-lg text-white custom-bg border border-white'>
-					2
+				<div className='service-item h-32 p-4 rounded-lg text-white custom-bg border border-white'>
+					<FontAwesomeIcon
+						className='css-icon text-6xl hover:text-7xl'
+						icon={faCss3}
+					/>
+					<h3 className='font-bold black-shadow text-xl '>
+						CSS Services
+					</h3>
 				</div>
-				<div className='service-item p-4 rounded-lg text-white custom-bg border border-white'>
-					3
+				<div className='service-item h-32 p-4 rounded-lg text-white custom-bg border border-white'>
+					<FontAwesomeIcon
+						className='bootstrap-icon text-6xl hover:text-7xl'
+						icon={faBootstrap}
+					/>
+					<h3 className='font-bold black-shadow text-xl '>
+						Bootstrap Services
+					</h3>
 				</div>
-				<div className='service-item p-4 rounded-lg text-white custom-bg border border-white'>
-					1
+				<div className='service-item h-32 p-4 rounded-lg text-white custom-bg border border-white'>
+					<FontAwesomeIcon
+						className='react-icon text-6xl hover:text-7xl'
+						icon={faReact}
+					/>
+					<h3 className='font-bold black-shadow text-xl '>
+						React Services
+					</h3>
 				</div>
-				<div className='service-item p-4 rounded-lg text-white custom-bg border border-white'>
-					2
+				<div className='service-item h-32 p-4 rounded-lg text-white custom-bg border border-white'>
+					<FontAwesomeIcon
+						className='bootstrap-icon text-6xl hover:text-7xl'
+						icon={faDesktop}
+					/>
+					<h3 className='font-bold black-shadow text-xl '>
+						HTML Services
+					</h3>
 				</div>
-				<div className='service-item p-4 rounded-lg text-white custom-bg border border-white'>
-					3
+				<div className='service-item h-32 p-4 rounded-lg text-white custom-bg border border-white'>
+					<FontAwesomeIcon
+						className='css-icon text-6xl hover:text-7xl'
+						icon={faLaptopCode}
+					/>
+					<h3 className='font-bold black-shadow text-xl '>
+						HTML Services
+					</h3>
 				</div>
 			</div>
 		</div>
